40ed7ed4c6c6efcdcb0f6898df1d85934109f22c
Manuh08 <manuelasoarespacheco11@gmail.com>
2025-06-24 11:19:40 -0400

    Projeto pronto

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,18 @@
-// src/pages/_app.js
 import "@/styles/globals.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 
-// Criação do tema claro
+
 const themeClaro = createTheme({
   palette: {
-    mode: "light", // Força o tema claro
+    mode: "light",
   },
 });
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={themeClaro}>
-      <CssBaseline /> {/* Aplica o reset e estilo global do Material UI */}
+      <CssBaseline /> 
       <Component {...pageProps} />
     </ThemeProvider>
   );
